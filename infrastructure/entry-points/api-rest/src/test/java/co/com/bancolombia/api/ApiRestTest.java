e9e8b9b0aa7b4dc0ad218c1fc48895a7fecca695
maocq <johncarm@bancolombia.com.co>
2023-06-29 14:56:22 -0500

    ApiRest test

--- infrastructure/entry-points/api-rest/src/test/java/co/com/bancolombia/api/ApiRestTest.java
+++ infrastructure/entry-points/api-rest/src/test/java/co/com/bancolombia/api/ApiRestTest.java
@@ -1,20 +1,39 @@
 package co.com.bancolombia.api;
 
+import co.com.bancolombia.api.dto.request.RegisterAccountRequest;
+import co.com.bancolombia.model.account.Account;
+import co.com.bancolombia.usecase.registeraccount.RegisterAccountUseCase;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class ApiRestTest {
 
-    /*
-    ApiRest apiRest = new ApiRest();
-     */
+    @Mock
+    RegisterAccountUseCase registerAccountUseCase;
+    ApiRest apiRest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        apiRest = new ApiRest(registerAccountUseCase);
+    }
 
     @Test
-    void apiRestTest() {
-        /*
-        var response = apiRest.commandName();
-        assertEquals("Hello World", response);
-         */
+    void useCaseTest() {
+        when(registerAccountUseCase.register(anyLong(), anyString(), anyString()))
+                .thenReturn(Account.builder().id(99L).name("Foo").build());
+
+        apiRest = new ApiRest(registerAccountUseCase);
+        RegisterAccountRequest request = RegisterAccountRequest.builder().id(99L).name("Foo").statusId("id").build();
+        Account result = apiRest.commandName(request);
+
+        assertEquals("Foo", result.getName());
     }
 }
